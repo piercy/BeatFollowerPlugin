213eae5205fa981793ab1a3a488b5ea1ad561d09
Michael Pierce <michael.pierce08@gmail.com>
2020-08-22 22:52:50 +0100

    Dynamic list created for follow list implementation

--- BeatFollower/UI/BeatFollowerViewController.cs
+++ BeatFollower/UI/BeatFollowerViewController.cs
@@ -5,39 +5,53 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using BeatFollower.Models;
 using BeatSaberMarkupLanguage;
 using BeatSaberMarkupLanguage.Attributes;
 using BeatSaberMarkupLanguage.Components;
 using BeatSaberMarkupLanguage.ViewControllers;
-using UnityEngine;
 
 namespace BeatFollower.UI
 {
-    [HotReload(@"C:\working\BeatFollowerPlugin\BeatFollower\UI\BeatFollowerView.bsml")]
-    [ViewDefinition("BeatFollower.UI.BeatFollowerView.bsml")]
+    [HotReload(@"C:\working\BeatFollowerPlugin\BeatFollower\UI\FollowerList.bsml")]
+    [ViewDefinition("BeatFollower.UI.FollowerList.bsml")]
+
+
+
+
     public class BeatFollowerViewController : BSMLAutomaticViewController // BSMLResourceViewController
     {
-      //  public override string ResourceName => "BeatFollower.UI.BeatFollowerView.bsml";
-      [UIObject("ScrollContent")] internal GameObject ScrollContent;
+        [UIComponent("follower-list")]
+        public CustomCellListTableData followerList;
+
+        [UIValue("followers")]
+        public List<object> followersUiList = new List<object>();
 
         protected override void DidActivate(bool firstActivation, ActivationType activationType)
         {
-            Logger.log.Debug("Im in the VC");
             base.DidActivate(firstActivation, activationType);
-            
-            Logger.log.Debug("Im in the VC1");
-            for (int i = 0; i < 10; i++)
+
+            SetFollowers(new List<Follower>()
             {
-                var res = BeatSaberMarkupLanguage.Utilities.GetResourceContent(Assembly.GetExecutingAssembly(), "BeatFollower.UI.Follower.bsml");
-                if(res == null)
-                    Logger.log.Error("Res is null");
+                new Follower("PiercyTTV","https://static-cdn.jtvnw.net/jtv_user_pictures/cecc0819-b7df-4e7d-8db8-9695e241267c-profile_image-300x300.png"),
+                new Follower("Acetari","https://static-cdn.jtvnw.net/jtv_user_pictures/45c580d5-e3e5-4900-8309-c8816b16339a-profile_image-300x300.png"),
 
-                BSMLParser.instance.Parse(res, ScrollContent, this);
-                
-            }
-
-          
+            });
         }
+        public void SetFollowers(List<Follower> followers)
+        {
+            followersUiList.Clear();
 
+            if (followers != null)
+            {
+                foreach (var follower in followers)
+                {
+                    followersUiList.Add(new FollowerUiObject(follower.Twitch,follower.ProfileImageUrl ));
+                }
+            }
+            
+            followerList.tableView.ReloadData();
+
+        }
     }
 }
