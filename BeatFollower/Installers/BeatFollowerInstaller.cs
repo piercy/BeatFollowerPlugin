c47dde79ade8aeaee5ff9a04a1f1511c2d809cad
Auros Nexus <41306347+Auros@users.noreply.github.com>
2020-08-22 09:48:06 -0400

    zenjectified

---
+++ BeatFollower/Installers/BeatFollowerInstaller.cs
@@ -0,0 +1,16 @@
+﻿using BS_Utils.Utilities;
+using BeatFollower.Services;
+
+namespace BeatFollower.Installers
+{
+    public class BeatFollowerInstaller : Zenject.Installer
+    {
+        public override void InstallBindings()
+        {
+            string name = "BeatFollower";
+            Container.BindInterfacesAndSelfTo<EventService>().AsSingle();
+            Container.BindInterfacesAndSelfTo<BeatFollowerService>().AsSingle().NonLazy();
+            Container.BindInstance(new Config(name)).WithId($"{name} Config").AsSingle();
+        }
+    }
+}
---
+++ BeatFollower/Services/EventService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Zenject;
+
+namespace BeatFollower.Services
+{
+    public class EventService : IInitializable, IDisposable
+    {
+        private readonly GameScenesManager _gameScenesManager;
+
+        public event Action<IBeatmapLevel> LevelStarted;
+        public event Action<LevelCompletionResults> LevelFinished;
+
+        public EventService(GameScenesManager gameScenesManager)
+        {
+            _gameScenesManager = gameScenesManager;
+        }
+
+        private void GameScenesManager_transitionDidFinishEvent(ScenesTransitionSetupDataSO _, DiContainer container)
+        {
+            IDifficultyBeatmap beatmap = container.TryResolve<IDifficultyBeatmap>();
+            if (beatmap != null)
+            {
+                LevelStarted?.Invoke(beatmap.level);
+            }
+        }
+
+        public void Initialize()
+        {
+            BS_Utils.Plugin.LevelDidFinishEvent += OnLevelDidFinishEvent;
+            _gameScenesManager.transitionDidFinishEvent += GameScenesManager_transitionDidFinishEvent;
+        }
+
+        public void Dispose()
+        {
+            BS_Utils.Plugin.LevelDidFinishEvent += OnLevelDidFinishEvent;
+            _gameScenesManager.transitionDidFinishEvent -= GameScenesManager_transitionDidFinishEvent;
+        }
+
+        private void OnLevelDidFinishEvent(StandardLevelScenesTransitionSetupDataSO _, LevelCompletionResults levelCompletionResults)
+        {
+            LevelFinished?.Invoke(levelCompletionResults);
+        }
+    }
+}
