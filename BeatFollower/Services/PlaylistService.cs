fc426acf448ab5f777ce8d6a5c6aaea09873cbf1
Michael Pierce <michael.pierce08@gmail.com>
2020-10-12 21:38:36 +0100

    Fixed hardcoded playlist addresss

--- BeatFollower/Services/PlaylistService.cs
+++ BeatFollower/Services/PlaylistService.cs
@@ -25,16 +25,13 @@ namespace BeatFollower.Services
 
             SharedCoroutineStarter.instance.StartCoroutine(_requestService.Get($"feed/playlist/{playlistType}/{twitch}/BeatFollower{playlistType}-{twitch}.json", playlistJson =>
                 {
-                    Logger.log.Debug("PL: " + playlistJson.Length);
-                    var filePath = $@"G:\SteamLibrary\steamapps\common\Beat Saber\Playlists\BeatFollowerRecommended-{twitch}.json";
-
-                    File.WriteAllText(filePath, playlistJson);
-                    Logger.log.Debug("LOADING");
-
-                    PluginUI obj = Resources.FindObjectsOfTypeAll<PluginUI>().FirstOrDefault<PluginUI>();
-                    typeof(PluginUI).GetMethod("RefreshButtonPressed", BindingFlags.Instance | BindingFlags.NonPublic).Invoke(obj, null);
-
-
+                    var playlistFolderPath = $@"{Application.dataPath}\..\Playlists\";
+                    var playlistFileName = $"BeatFollowerRecommended-{twitch}.json";
+                    if (Directory.Exists(playlistFolderPath))
+                    {
+                        Logger.log.Debug("Writing Playlist: " + playlistFolderPath + playlistFileName + " Length: " + playlistJson.Length);
+                        File.WriteAllText(playlistFolderPath + playlistFileName, playlistJson);
+                    }
 
                 }));
             
